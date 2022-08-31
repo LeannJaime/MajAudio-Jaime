aa2b6401cb4759773f1ebebf6a13ba57f0b2fcc8
Leandro Jaime <lean.jaime@gmail.com>
2022-08-30 23:34:56 -0300

    agregué NavBar

---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './styles.css';
+
+const NavBar = () => {
+  return (
+    <>
+        <h1>MAJ AUDIO</h1>
+        <ul>
+    <li><a href="#inicio">Inicio</a></li>
+    <li><a href="#productos">Productos</a></li>
+    <li><a href="#contacto">Contacto</a></li>
+    <li><a href="#nosotros">Nosotros</a></li>
+    </ul>
+    </> 
+ )
+}
+
+export default NavBar
