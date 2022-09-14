8ca517117250ab8653548c3f85c8497a4214e55b
Leandro Jaime <lean.jaime@gmail.com>
2022-09-14 00:03:55 -0300

    correción de imagen-detail

--- src/components/ItemDetail/index.jsx
+++ src/components/ItemDetail/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { imgProdcutos } from '../../data/imagenes';
 import './styles.css';
 
 const ItemDetail = ({product}) => {
     return (
     <div className='detail'>
         <h1>{product.name}</h1>
-        <img src={product.id} alt="foto-producto"/>
+        <img className='img-detail' src={imgProdcutos(`./${product.id}.jpg`)} alt="foto-producto"/>
         <h3>{product.description}</h3>
         <p>${product.precio}</p>
     </div>
